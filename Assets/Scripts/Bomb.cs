d2b23393f7b20fecad3b3743d093d6646f3da2ff
Alex Dreamworker <alexdreamworker@gmail.com>
2022-01-10 20:14:41 +0300

    Передвижение игрока, создание бомбы

---
+++ Assets/Scripts/Bomb.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bomb : MonoBehaviour
+{
+    public float countdown = 3f;
+
+    private void Update()
+    {
+        countdown -= Time.deltaTime;
+
+        if (countdown <= 0f)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
